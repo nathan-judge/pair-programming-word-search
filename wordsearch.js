7139a19e11b109e23b151700e7e0709a4024e74f
nathan-judge <nathanjudge27@gmail.com>
2021-09-22 23:10:20 +0000

    problem solved

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,34 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = function (matrix) {
+    let newArr = []
+
+    for (let i = 0; i < matrix[0].length; i++) {
+        newArr.push([]);
     }
-}
+    for (let r = 0; r < matrix.length; r++) {
+        for (let c = 0; c < matrix[r].length; c++) {
+            newArr[c][r] = matrix[r][c]
+        }
+    }
+    return newArr
+};
+
+
+
+
+const findWord = (letters, word) => {
+    const newWords = letters.map(ls => ls.join(''));
+    for (const letters of newWords) {
+        if (letters.includes(word)) return true;
+    }
+};
+
+const wordSearch = (letters, word) => {
+    if (findWord(letters, word)) {
+        return true;
+    } else if (findWord(transpose(letters), word)) {
+        return true;
+    }
+    return false;
+};
 
 module.exports = wordSearch
